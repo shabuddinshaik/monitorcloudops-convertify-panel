9fed4be36d59cff92561e348e2a32eeb71064682
shabuddinshaik <mr.shabuddinshaik@gmail.com>
2026-02-21 09:37:41 +0530

    updated panel specs

--- tests/panel.spec.ts
+++ tests/panel.spec.ts
@@ -4,7 +4,13 @@ test('should display "No data" in case panel data is empty', async ({
   gotoPanelEditPage,
   readProvisionedDashboard,
 }) => {
+  test.setTimeout(120_000);
   const dashboard = await readProvisionedDashboard({ fileName: 'dashboard.json' });
-  const panelEditPage = await gotoPanelEditPage({ dashboard, id: '2' });
+  const panelEditPage = await gotoPanelEditPage({
+    dashboard,
+    id: '2',
+    waitUntil: 'domcontentloaded',
+    timeout: 90_000,
+  });
   await expect(panelEditPage.panel.locator).toContainText('No data');
 });
